5e692028ca2b15f12013707ae50a04f98f2e1012
isharab <sherijigarx@gmail.com>
2024-04-01 17:11:17 +0500

    Update text_to_speech_models.py

--- models/text_to_speech_models.py
+++ models/text_to_speech_models.py
@@ -2,15 +2,12 @@
 # Copyright © 2023 Yuma Rao
 # (developer): ETG development team
 # Copyright © 2023 ETG
-
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 # and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of
 # the Software.
-
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
@@ -24,15 +21,6 @@ import random
 import torch
 import os
 import sys
-# Set the project root path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-# Set the 'AudioSubnet' directory path
-audio_subnet_path = os.path.abspath(project_root)
-# Add the project root and 'AudioSubnet' directories to sys.path
-sys.path.insert(0, project_root)
-sys.path.insert(0, audio_subnet_path)
-
-
 # Text-to-Speech Generation Using Suno Bark's Pretrained Model
 class SunoBark:
     def __init__(self, model_path="suno/bark"):
@@ -46,33 +34,26 @@ class SunoBark:
         # Process the text wit speaker
         speaker = self.speaker_list[torch.multinomial(torch.ones(len(self.speaker_list)), 1).item()]
         inputs = self.processor(text_input, voice_preset= speaker, return_tensors="pt")
-
         # Move inputs to the same device as model
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
-
         # Generate audio
         speech = self.model.generate(**inputs)
         return speech
     
-
 class EnglishTextToSpeech:
     def __init__(self, model_path="facebook/mms-tts-eng"):
         self.model = VitsModel.from_pretrained(model_path)
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         self.device = torch.device("cuda")
-
-
     def generate_speech(self, text_input):
         inputs = self.tokenizer(text_input, return_tensors="pt")
         with torch.no_grad():
             speech = self.model(**inputs).waveform
         return speech
-
 class ElevenLabsTTS:
     def __init__(self, api_key):
         self.api_key = api_key
         self.voices = voices()
-
     def generate_speech(self,text_input):
         selected_voice = None
         audio = None
@@ -83,28 +64,24 @@ class ElevenLabsTTS:
                 text = text_input,
                 voice=selected_voice.voice_id,
             )
-
             return audio
         except Exception as e:
             print(f"An error occurred while processing the input audio in 11 Labs: {e}")
-
 class MeloTTS:
     def __init__(self, model_path="myshell-ai/MeloTTS-English"):
         self._setup_environment()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = self._load_model()
         self.speaker_ids = self.model.hps.data.spk2id
-
     def _setup_environment(self):
         if not os.path.isdir('MeloTTS'):
             os.system('git clone https://github.com/myshell-ai/MeloTTS.git')
             os.system('python -m unidic download')
-        sys.path.append(audio_subnet_path + 'MeloTTS/')
+        sys.path.append('/root/tts_imp/MeloTTS/')
 
     def _load_model(self):
         from melo.api import TTS
         return TTS(language='EN', device=str(self.device))
-
     def generate_speech(self, text_input):
         speech = self.model.tts_to_file(text_input, self.speaker_ids['EN-US'])
         return speech
